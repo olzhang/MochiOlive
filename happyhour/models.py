5b076461ba66b7b0d01b471c1bd7cd4c6de99472
System Administrator <root@dhcp-128-189-245-233.ubcsecure.wireless.ubc.ca>
2015-10-26 21:33:05 -0700

    Renamed lat and long into location_lat and location_long

--- happyhour/models.py
+++ happyhour/models.py
@@ -5,6 +5,6 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=200,null=True)
     phone_number = models.CharField(max_length=20,null=True)
     rating  = models.IntegerField(null=True)
-    lat = models.FloatField(null=True)
-    long = models.FloatField(null=True)
+    location_lat = models.FloatField(null=True)
+    location_long = models.FloatField(null=True)
     image_url = models.URLField(max_length=200, null=True)
