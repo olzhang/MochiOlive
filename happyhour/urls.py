9150245db6559a7ede8ee2693ffff84ed5837aa3
Nancy Cheng <ncboodah@Nancys-MacBook-Air.local>
2015-10-27 00:53:27 -0700

    Added API endpoints for restaurant detail specified by id number.

--- happyhour/urls.py
+++ happyhour/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^v1/restaurants/$', views.RestaurantList.as_view()),
-    # url(r'^/v1/restaurants/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^v1/restaurants/(?P<pk>[0-9]+)/$', views.RestaurantDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- happyhour/views.py
+++ happyhour/views.py
@@ -24,3 +24,33 @@ class RestaurantList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RestaurantDetail(APIView):
+    """
+    GET: get specific restaurant based on id
+    PUT: update specific restaurant based on id
+    DELETE: delete a restaurant specified by id
+    """
+    def get_object(self, pk):
+        try:
+            return Restaurant.objects.get(pk=pk)
+        except Restaurant.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        restaurant = self.get_object(pk)
+        serializer = RestaurantSerializer(restaurant)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        restaurant = self.get_object(pk)
+        serializer = RestaurantSerializer(restaurant, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        restaurant = self.get_object(pk)
+        restaurant.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
