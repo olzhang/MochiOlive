25652f76d16680965cb9cfab083130751e8982f0
kimmychen <xochoco@hotmail.com>
2015-10-26 21:29:43 -0700

    Added Restaurant Model

--- happyhour/models.py
+++ happyhour/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name  = models.CharField(max_length=200,null=True)
+    address = models.CharField(max_length=200,null=True)
+    phone_number = models.CharField(max_length=20,null=True)
+    rating  = models.IntegerField(null=True)
+    lat = models.FloatField(null=True)
+    long = models.FloatField(null=True)
+    image_url = models.URLField(max_length=200, null=True)
