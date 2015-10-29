972170c1a4e0fbb4f49ff8e8c63a46719865c2bf
Nancy Cheng <ncboodah@dhcp-128-189-79-130.ubcsecure.wireless.ubc.ca>
2015-10-29 03:12:56 -0700

    Changed rating field to allow decimals.

--- happyhour/models.py
+++ happyhour/models.py
@@ -4,7 +4,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=200,null=True)
     address = models.CharField(max_length=200,null=True)
     phone_number = models.CharField(max_length=20,null=True)
-    rating = models.IntegerField(null=True)
+    rating = models.DecimalField(null=True, decimal_places=1, max_digits=5)
     location_lat = models.FloatField(null=True)
     location_long = models.FloatField(null=True)
     image_url = models.URLField(max_length=200, null=True)
