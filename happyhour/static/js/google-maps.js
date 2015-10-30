a1537af58e5b238682ecffad23a93150d7978097
Nancy Cheng <ncboodah@Nancys-MacBook-Air.local>
2015-10-29 21:15:23 -0700

    Added marker to map as a starter.

--- happyhour/static/js/google-maps.js
+++ happyhour/static/js/google-maps.js
@@ -7,6 +7,12 @@ var Mapping;
             this.options = { center: { lat: 49.25, lng: -123.1 }, zoom: 10 };
             this.map = new google.maps.Map(mapDiv, this.options);
         }
+        GoogleMap.prototype.marker = function () {
+            var position = new google.maps.LatLng(49.25, -123.1);
+            var title = "First marker!";
+            var marker = new google.maps.Marker({ 'position': position, 'title': title });
+            marker.setMap(this.map);
+        };
         return GoogleMap;
     })();
     Mapping.GoogleMap = GoogleMap;
--- happyhour/static/js/google-maps.ts
+++ happyhour/static/js/google-maps.ts
@@ -3,13 +3,20 @@ module Mapping {
     export class GoogleMap {
 
         public name: string;
-        private map: any;
+        public map: any;
         private options: any;
 
         constructor (mapDiv:Element) {
             this.name = "GoogleMap";
             this.options = { center: {lat: 49.25, lng: -123.1}, zoom: 10 };
             this.map = new google.maps.Map(mapDiv, this.options);
+         }
+
+         marker(){
+            var position = new google.maps.LatLng(49.25, -123.1);
+            var title = "First marker!";
+            var marker = new google.maps.Marker({'position': position, 'title': title});
+            marker.setMap(this.map);
          }
     }
 }
