96e9241ed7c3635395e944aef5534948b4faa08e
Nancy Cheng <ncboodah@Nancys-MacBook-Air.local>
2015-10-31 12:04:46 -0700

    Changing typescript code back to javascript.

--- happyhour/static/js/google-maps.js
+++ happyhour/static/js/google-maps.js
@@ -1,19 +1,17 @@
-/// <reference path="../ts/google.maps.d.ts.1.0.10/content/typings/google.maps.d.ts" />
-var Mapping;
-(function (Mapping) {
-    var GoogleMap = (function () {
-        function GoogleMap(mapDiv) {
-            this.name = "GoogleMap";
-            this.options = { center: { lat: 49.25, lng: -123.1 }, zoom: 10 };
-            this.map = new google.maps.Map(mapDiv, this.options);
-        }
-        GoogleMap.prototype.marker = function () {
-            var position = new google.maps.LatLng(49.25, -123.1);
-            var title = "First marker!";
-            var marker = new google.maps.Marker({ 'position': position, 'title': title });
-            marker.setMap(this.map);
-        };
-        return GoogleMap;
-    })();
-    Mapping.GoogleMap = GoogleMap;
-})(Mapping || (Mapping = {}));
+// Initialize google maps
+var map;
+function initGoogleMap() {
+    var options={
+        center: {lat: 49.25, lng: -123.1},
+        zoom: 10
+    };
+  map = new google.maps.Map(document.getElementById('map'), options);
+}
+
+function markPoint(){
+    var latlng = new google.maps.LatLng(49.25, -123.1);
+    var comment = "New point!";
+    var options = {position: latlng, title: comment};
+    var marker = new google.maps.Marker(options);
+    marker.setMap(map);
+}
