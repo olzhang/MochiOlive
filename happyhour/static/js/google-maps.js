753c4dc948acd4dc80cdc21ef367f0f5bb5a83d0
Nancy Cheng <ncboodah@dhcp-128-189-74-54.ubcsecure.wireless.ubc.ca>
2015-10-31 22:34:45 -0700

    Added markers for all happy hour restaurants onto maps.

--- happyhour/static/js/google-maps.js
+++ happyhour/static/js/google-maps.js
@@ -5,13 +5,37 @@ function initGoogleMap() {
         center: {lat: 49.25, lng: -123.1},
         zoom: 10
     };
-  map = new google.maps.Map(document.getElementById('map'), options);
+    map = new google.maps.Map(document.getElementById('map'), options);
 }
 
+// Mark all happy hour restaurants on map
 function markPoint(){
-    var latlng = new google.maps.LatLng(49.25, -123.1);
-    var comment = "New point!";
-    var options = {position: latlng, title: comment};
-    var marker = new google.maps.Marker(options);
-    marker.setMap(map);
+    var restaurants = getData();
+    console.log(restaurants[0].id); 
+    for (key in restaurants){
+        var restaurant = restaurants[key];
+        var lat = restaurant.location_lat;
+        var lng = restaurant.location_long;
+        var latlng = new google.maps.LatLng(lat, lng);
+        var options = {position: latlng, title: restaurant.name};
+        var marker = new google.maps.Marker(options);
+        marker.setMap(map);
+    }
+}
+
+// Get happy hour restaurant data
+function getData(){
+    var items = {};
+    $.ajax({
+        url: 'http://127.0.0.1:8000/v1/restaurants/',
+        async: false,
+        dataType: 'json',
+        success: function (restaurants) {
+            $.each( restaurants, function( key, data ) {
+                items[key] = data;
+            });
+        }
+    });
+  console.log(items);
+  return items;
 }
