e9f9da3f7d7ea006499b79b11d511cc340bd2241
Ayana Matsui <putihonya@gmail.com>
2015-11-01 15:44:40 -0800

    Initial commit for pop-up(indowindow) when we click on the pin.
    -Currently should show 1 pop-up at a time

--- happyhour/static/js/google-maps.js
+++ happyhour/static/js/google-maps.js
@@ -1,11 +1,38 @@
 // Initialize google maps
 var map;
+var prev_infowindow;
+
 function initGoogleMap() {
     var options={
         center: {lat: 49.25, lng: -123.1},
         zoom: 11
     };
     map = new google.maps.Map(document.getElementById('map'), options);
+}
+
+//attach the infowindow to marker
+function bindInfoWindow(marker, map, infowindow, description) {
+    google.maps.event.addListener(marker, 'click', function() {
+        closePreviousInfoWindow();
+        if(prev_infowindow == infowindow) {
+           prev_infowindow.close();
+           prev_infowindow = null;
+           return;
+        }
+        prev_infowindow = infowindow;
+        infowindow.setContent(description);
+        infowindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(map, 'click', function() {
+        infowindow.close();
+    });
+}
+
+function closePreviousInfoWindow(){
+     if( prev_infowindow ) {
+           prev_infowindow.close();
+        }
 }
 
 // Mark all happy hour restaurants on map
@@ -20,9 +47,12 @@ function markPoint(){
         var latlng = new google.maps.LatLng(lat, lng);
         var options = {position: latlng, title: restaurant.name};
         var marker = new google.maps.Marker(options);
+        var infowindow =  new google.maps.InfoWindow();
         // marker.setMap(map);
+
+        bindInfoWindow(marker, map, infowindow, restaurant.name);
         markers.push(marker);
-    }
+  };
     var cluster = new MarkerClusterer(map, markers);
 }
 
