a146d49e426faf260aa2740234ee575be80646e6
olzhang <oliver.zhang2718@gmail.com>
2015-11-03 12:17:50 -0800

    added admin page and login

---
+++ happyhour/admin_view.py
@@ -0,0 +1,11 @@
+from happyhour.models import Restaurant
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='happy-hour-restaurants/login/admin')
+def restaurant_list_admin(request):
+    """
+    gets information about all happy hour restaurants and sends to template for display.
+    """
+    restaurants = Restaurant.objects.order_by('-rating')
+    return render(request, 'happyhour/admin_base.html', {'restaurants': restaurants})
--- happyhour/urls.py
+++ happyhour/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from happyhour import views, restaurant_views
+from happyhour import views, restaurant_views, admin_view
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^v1/restaurants/$', views.RestaurantList.as_view()),
     url(r'^v1/restaurants/(?P<pk>[0-9]+)/$', views.RestaurantDetail.as_view()),
-    url(r'^happy-hour-restaurants/$', restaurant_views.restaurant_list, name='restaurant_list')
+    url(r'^happy-hour-restaurants/$', restaurant_views.restaurant_list, name='restaurant_list'),
+    url(r'^happy-hour-restaurants/login/admin$', 'django.contrib.auth.views.login', {'template_name': 'happyhour/login.html'}),
+    url(r'^happy-hour-restaurants/admin/dashboard/$', admin_view.restaurant_list_admin, name='restaurant_list_admin')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
