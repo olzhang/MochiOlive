229479cf56e0a816a372b11897579e6be204d03d
olzhang <oliver.zhang2718@gmail.com>
2015-11-03 20:38:41 -0800

    updated the login functionality so that:
    a) we have a "login as admin" functionality at the home page
    b) now you actually need to login to use the dashboard

--- happyhour/urls.py
+++ happyhour/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
 from happyhour import views, restaurant_views, admin_view
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^v1/restaurants/$', views.RestaurantList.as_view()),
     url(r'^v1/restaurants/(?P<pk>[0-9]+)/$', views.RestaurantDetail.as_view()),
     url(r'^happy-hour-restaurants/$', restaurant_views.restaurant_list, name='restaurant_list'),
-    url(r'^login/admin$', 'django.contrib.auth.views.login', {'template_name': 'happyhour/login.html'}),
-    url(r'^admin/dashboard/$', admin_view.restaurant_list_admin, name='restaurant_list_admin')
+    url(r'^login/admin/$', 'django.contrib.auth.views.login', {'template_name': 'happyhour/login.html'}),
+    url(r'^admin/dashboard/$', admin_view.restaurant_list_admin, name='restaurant_list_admin'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/happy-hour-restaurants/'})
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
