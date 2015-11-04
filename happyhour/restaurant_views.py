c5d36e77312f30f132cf2ed022a4cbf7d40c024f
Nancy Cheng <ncboodah@dhcp-128-189-78-21.ubcsecure.wireless.ubc.ca>
2015-11-04 00:06:35 -0800

    Changed restaurant views to be class based.

--- happyhour/restaurant_views.py
+++ happyhour/restaurant_views.py
@@ -1,10 +1,14 @@
 from happyhour.models import Restaurant
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
+class RestaurantPage(ListView):
+	queryset=Restaurant.objects.order_by("-rating")
+	model = Restaurant
 
-def restaurant_list(request):
-    """
-    gets information about all happy hour restaurants and sends to template for display.
-    """
-    restaurants = Restaurant.objects.order_by('-rating')
-    return render(request, 'happyhour/restaurant_base.html', {'restaurants': restaurants})
+	def get_context_data(self, **kwargs):
+	    """
+	    gets information about all happy hour restaurants and sends to template for display.
+	    """
+	    context = super(RestaurantPage, self).get_context_data(**kwargs)
+	    return context
--- happyhour/urls.py
+++ happyhour/urls.py
@@ -5,7 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^v1/restaurants/$', views.RestaurantList.as_view()),
     url(r'^v1/restaurants/(?P<pk>[0-9]+)/$', views.RestaurantDetail.as_view()),
-    url(r'^happy-hour-restaurants/$', restaurant_views.restaurant_list, name='restaurant_list'),
+    url(r'^happy-hour-restaurants/$', restaurant_views.RestaurantPage.as_view(), name='restaurant-base'),
     url(r'^happy-hour-restaurants/login/admin$', 'django.contrib.auth.views.login', {'template_name': 'happyhour/login.html'}),
     url(r'^happy-hour-restaurants/admin/dashboard/$', admin_view.restaurant_list_admin, name='restaurant_list_admin')
 ]
