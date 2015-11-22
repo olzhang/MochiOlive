e69008778b0e5e44e9e6d6286f59ab3e997028a5
Ayana Matsui <putihonya@gmail.com>
2015-11-22 13:04:59 -0800

    Showing message on successful login with twitter
    When the user successfully logs in with their twitter account,
    the pop up appears(which can be removed by closing it with x button)
    
    The empty.txt file is for overwriting existing django message.
    Maybe there is a better place to put receiver or ways to making it in to class?

--- happyhour/models.py
+++ happyhour/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from allauth.account.signals import user_logged_in
+from django.dispatch import receiver
+from django.contrib import messages
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=200,null=True)
     address = models.CharField(max_length=200,null=True)
@@ -14,4 +18,6 @@ class Favorites(models.Model):
 	user = models.ForeignKey(User)
 	restaurant = models.ForeignKey(Restaurant)
 
-
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):
+    messages.success(request, ("Thank you for signing in with twitter account %(username)s") % {'username': user.username},)
