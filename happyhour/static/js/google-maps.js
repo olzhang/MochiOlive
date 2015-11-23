07a1582d262b950475bd2d9a3f7e6c7f7bbadf77
Nancy Cheng <ncboodah@Nancys-MacBook-Air.local>
2015-11-22 17:50:14 -0800

    Fixing url to be relative url.

--- happyhour/static/js/google-maps.js
+++ happyhour/static/js/google-maps.js
@@ -168,7 +168,7 @@ function markPoint(){
 function getData(){
     var items = {};
     $.ajax({
-        url: 'http://127.0.0.1:8000/v1/restaurants/',
+        url: '/v1/restaurants/',
         async: false,
         dataType: 'json',
         success: function (restaurants) {
