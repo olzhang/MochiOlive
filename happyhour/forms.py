29a3ce9f7fd19b8ec1d89ad5b77a9ea45a2c4ab5
olzhang <oliver.zhang2718@gmail.com>
2015-11-24 13:43:15 -0800

    added some crap that displays better error messages

--- happyhour/forms.py
+++ happyhour/forms.py
@@ -9,9 +9,21 @@ class RegistrationForm(UserCreationForm):
 		model = User
 		fields = ("username", "email", "password1", "password2")
 
+	def clean_password2(self):
+	    password1 = self.cleaned_data.get('password1')
+	    password2 = self.cleaned_data.get('password2')
+
+	    if not password2:
+	        raise forms.ValidationError("You must confirm your password")
+	    if password1 != password2:
+	        raise forms.ValidationError("Your passwords do not match")
+	    return password2
+
 	def save(self, commit=True):
 		user = super(RegistrationForm, self).save(commit=False)
+		user.username = self.cleaned_data["username"]
 		user.email = self.cleaned_data["email"]
+		self.clean_password2()
 
 		if commit:
 			user.save()
